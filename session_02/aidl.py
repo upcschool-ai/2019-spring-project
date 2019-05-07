22eb24f99b022ab73ece06a6477168903e9227c2
Issey Masuda <issey@vilynx.com>
2019-05-07 03:27:02 +0200

    S02: Some changes

--- session_02/aidl.py
+++ session_02/aidl.py
@@ -3,21 +3,20 @@ import numpy as np
 
 class SampleGenerator(object):
     def __init__(self):
-        self.W = np.random.uniform(-10, 10)  # Set the weight to be a random number between [-10, 10]
+        self.W = np.random.uniform(-5, 5)  # Set the weight to be a random number between [-5, 5]
         self.b = np.random.uniform(-5, 5)  # Set the bias to be a random number between [0, 5]
 
-    def __call__(self, num_iters, batch_size=1):
+    def __call__(self, num_iters):
         for i in xrange(num_iters):
-            x = np.random.uniform(-100, 100, size=batch_size)  # Draw random input from [-100, 100]
+            x = np.random.uniform(-200, 200)  # Draw random input from [-100, 100]
             y = self.W * x + self.b  # Compute output for current input
             yield x, y
 
 
-def sample_generator(num_iters, batch_size=1):
+def sample_generator(num_iters):
     """Generator for samples of the "hidden" data distribution
 
     :param num_iters:
-    :param batch_size:
-    :return: tuples of (x, y). If batch_size > 1, x and y are arrays with batch dimension
+    :return: tuples of (x, y)
     """
-    return SampleGenerator()(num_iters, batch_size=batch_size)
+    return SampleGenerator()(num_iters)
--- session_02/exercise.py
+++ session_02/exercise.py
@@ -3,7 +3,10 @@ from abc import ABCMeta, abstractmethod
 
 
 class Operation(object):
-    """Base class that holds the interface for operations in our graph"""
+    """
+    Base class that holds the interface for operations in our graph.
+    As this is a toy example, it's only defined for N:1 operations (multiple input, single output)
+    """
     __metaclass__ = ABCMeta
 
     @abstractmethod
@@ -12,9 +15,8 @@ class Operation(object):
         pass
 
     @abstractmethod
-    def backward(self, *gradients):
-        """Compute the outgoing gradient given the flowing gradients.
-        Each input from the forward pass must have an output gradient here"""
+    def backward(self, gradient):
+        """Compute the outgoing gradient and any intermediate one given the flowing gradients"""
         pass
 
 
@@ -27,7 +29,7 @@ class LinearRegressor(Operation):
         # TODO: compute the forward pass
         pass
 
-    def backward(self, *gradients):
+    def backward(self, gradient):
         # TODO: compute the outgoing gradients
         pass
 
@@ -38,7 +40,7 @@ class MSELoss(Operation):
         # TODO: compute the loss given the predictions
         pass
 
-    def backward(self, *gradients):
+    def backward(self, gradient):
         # TODO: compute the gradient for the predictions
         pass
 
@@ -52,18 +54,15 @@ class SGDOptimizer(object):
 
 
 def main(learning_rate):
-    model = LinearRegressor()
-    loss = MSELoss()
-    optimizer = SGDOptimizer(learning_rate=learning_rate)
-
     # TODO: definition phase
 
     # TODO: run phase
+    pass
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-lr', '--learning_rate', type=float, default=10e-3,
+    parser.add_argument('-lr', '--learning_rate', type=float, default=10e-6,
                         help='Learning rate for the optimization step')
     args = parser.parse_args()
     main(args.learning_rate)
