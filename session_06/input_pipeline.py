0b8565934048d503052c3aad30db819918cad100
Issey Masuda <issey@vilynx.com>
2019-06-04 18:40:42 +0200

    S06: Add filewriter

--- session_06/input_pipeline.py
+++ session_06/input_pipeline.py
@@ -59,13 +59,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Pipeline execution')
     parser.add_argument('dataset_path', help='Path to dataset description')
     parser.add_argument('images_dir', help='Image directory')
+    parser.add_argument('-l', '--logdir', default='~/tmp/aidl', help='Log dir for tfevents')
     parser.add_argument('-e', '--num_epochs', type=int, default=1, help='Number of epochs')
     parser.add_argument('-b', '--batch_size', type=int, default=5, help='Batch size')
     args = parser.parse_args()
 
-    dataset = create_dataset(args.dataset_path, args.images_dir, args.num_epochs, args.batch_size)
-    iterator = dataset.make_one_shot_iterator()
-    batch = iterator.get_next()
+    with tf.device('/cpu:0'):
+        with tf.name_scope('input_pipeline'):
+            dataset = create_dataset(args.dataset_path, args.images_dir, args.num_epochs, args.batch_size)
+            iterator = dataset.make_one_shot_iterator()
+            batch = iterator.get_next()
 
     with tf.Session() as sess:
         try:
@@ -74,3 +77,8 @@ if __name__ == '__main__':
                 print images.shape, labels.shape
         except tf.errors.OutOfRangeError:
             pass
+
+    logdir = os.path.expanduser(args.logdir)
+    if not os.path.isdir(logdir):
+        os.makedirs(logdir)
+    writer = tf.summary.FileWriter(logdir, graph=tf.get_default_graph())
