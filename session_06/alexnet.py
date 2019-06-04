b62458078ce2cd8db476e49127fe6b770961af1e
Issey Masuda <issey@vilynx.com>
2019-06-04 20:00:17 +0200

    S06: Exercise 5

---
+++ session_06/alexnet.py
@@ -0,0 +1,16 @@
+"""
+Implement AlexNet: https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf
+"""
+import argparse
+
+
+def main():
+    # TODO: implement AlexNet
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train AlexNet')
+
+    args = parser.parse_args()
+    main()
