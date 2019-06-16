485c4b362f9b252bc0421da517c49d2d89ca01bc
Santi Puch <santipuch590@gmail.com>
2019-06-16 14:51:26 +0200

    WIP: AlexNet solution

--- session_08/alexnet.py
+++ session_08/alexnet.py
@@ -6,6 +6,8 @@ https://www.tensorflow.org/api_docs/python/tf/nn
 https://www.tensorflow.org/api_docs/python/tf/layers
 https://www.tensorflow.org/api_docs/python/tf/keras/layers
 """
+from __future__ import print_function
+
 import argparse
 import os
 
@@ -14,17 +16,24 @@ import tensorflow as tf
 import input_pipeline
 
 
-def main(dataset_path, images_dir, num_epochs, batch_size, logdir):
+def main(dataset_csv, dataset_dir, num_epochs, batch_size, logdir):
+    # ----------------- TRAINING LOOP SETUP ---------------- #
+    logdir = os.path.expanduser(logdir)
+    if not os.path.isdir(logdir):
+        os.makedirs(logdir)
+    writer = tf.summary.FileWriter(logdir, graph=tf.get_default_graph())
+
     # ----------------- DEFINITION PHASE ------------------- #
-    global_step = tf.get_variable('global_step', shape=[], dtype=tf.int32, initializer=0, trainable=False)
+    global_step = tf.get_variable('global_step', dtype=tf.int32, initializer=0, trainable=False)
+
     # Input pipeline
     with tf.device('/cpu:0'):
         with tf.name_scope('input_pipeline'):
-            dataset = input_pipeline.create_dataset(dataset_path, images_dir, num_epochs, batch_size)
+            dataset = input_pipeline.create_dataset(dataset_csv, dataset_dir, num_epochs, batch_size)
             iterator = dataset.make_one_shot_iterator()
             images, labels = iterator.get_next()
 
-    # Model definition
+    # Model
     conv1 = conv_layer(images, filters=96, kernel_size=(11, 11), strides=(4, 4), lrn=True, max_pool=True, scope='conv1')
     conv2 = conv_layer(conv1, filters=256, kernel_size=(5, 5), lrn=True, max_pool=True, scope='conv2')
     conv3 = conv_layer(conv2, filters=385, kernel_size=(3, 3), scope='conv3')
@@ -36,25 +45,22 @@ def main(dataset_path, images_dir, num_epochs, batch_size, logdir):
     logits = fully_connected(fc2, units=1000, scope='fc3')
 
     # Loss
-    loss = tf.losses.softmax_cross_entropy(labels, logits)
+    loss_op = tf.losses.sparse_softmax_cross_entropy(labels, logits)
 
     # Optimizer
     optimizer = tf.train.MomentumOptimizer(learning_rate=0.1, momentum=0.9)
-    train_step = optimizer.minimize(loss, global_step=global_step)
+    train_step = optimizer.minimize(loss_op, global_step=global_step)
 
     # ----------------- RUN PHASE ------------------- #
     with tf.Session() as sess:
         try:
             while True:
-                _, loss_value, iteration = sess.run([train_step, loss, global_step])
-                print 'Iteration [{}]. Loss: {}'.format(iteration, loss_value)
+                # Run the train step
+                _, loss, step = sess.run([train_step, loss_op, global_step])
+                # Print how the loss is evolving per step in order to check if the model is converging
+                print('Step {}\tLoss={}'.format(step, loss))
         except tf.errors.OutOfRangeError:
             pass
-
-    logdir = os.path.expanduser(logdir)
-    if not os.path.isdir(logdir):
-        os.makedirs(logdir)
-    writer = tf.summary.FileWriter(logdir, graph=tf.get_default_graph())
 
 
 def conv_layer(inputs, filters, kernel_size, strides=(1, 1), lrn=False, max_pool=False, scope='conv_layer'):
@@ -78,12 +84,12 @@ def fully_connected(inputs, units, activation=tf.nn.relu, dropout=None, scope='f
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Pipeline execution')
-    parser.add_argument('dataset_path', help='Path to dataset description')
-    parser.add_argument('images_dir', help='Image directory')
+    parser = argparse.ArgumentParser(description='AlexNet model training')
+    parser.add_argument('dataset_csv', help='Path to the CSV decribing the dataset')
+    parser.add_argument('dataset_dir', help='Directory where the csv and the images folder are located')
     parser.add_argument('-l', '--logdir', default='~/tmp/aidl', help='Log dir for tfevents')
     parser.add_argument('-e', '--num_epochs', type=int, default=1, help='Number of epochs')
     parser.add_argument('-b', '--batch_size', type=int, default=5, help='Batch size')
     args = parser.parse_args()
 
-    main(args.dataset_path, args.images_dir, args.num_epochs, args.batch_size, args.logdir)
+    main(args.dataset_csv, args.dataset_dir, args.num_epochs, args.batch_size, args.logdir)
