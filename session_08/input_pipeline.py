d3eacef293256b540d5af7b56210d81b77fe9eca
Santi Puch <santipuch590@gmail.com>
2019-06-17 20:36:38 +0200

    Input pipeline ready for alexnet exercise

--- session_08/input_pipeline.py
+++ session_08/input_pipeline.py
@@ -16,42 +16,43 @@ import numpy as np
 import tensorflow as tf
 
 
-def create_dataset(dataset_csv, dataset_dir, num_epochs, batch_size):
-    dataset = tf.data.Dataset.from_generator(lambda: _generator(dataset_csv, dataset_dir),
+def create_dataset(dataset_csv, images_dir, num_epochs, batch_size):
+    dataset = tf.data.Dataset.from_generator(lambda: _generator(dataset_csv, images_dir),
                                              output_types=(tf.string, tf.string),
                                              output_shapes=(tf.TensorShape([]), tf.TensorShape([])))
     dataset = dataset.repeat(num_epochs)
     dataset = dataset.shuffle(100)  # Shuffling buffer
-    dataset = dataset.map(_create_sample_cifar10, num_parallel_calls=multiprocessing.cpu_count())
+    dataset = dataset.map(_create_sample, num_parallel_calls=multiprocessing.cpu_count())
     dataset = dataset.batch(batch_size=batch_size)
     dataset = dataset.prefetch(10)  # Pipelining
     return dataset
 
 
-def _generator(path, dataset_dir):
+def _generator(path, images_dir):
     with open(path) as f:
         reader = csv.reader(f)
         for label, image_path in reader:
-            image_path = os.path.join(dataset_dir, image_path)
+            image_path = os.path.join(images_dir, image_path)
             yield image_path, label
 
 
-def _create_sample_cifar10(image_path, label):
+def _create_sample(image_path, label):
     with tf.name_scope('create_sample'):
         with tf.name_scope('read_image'):
             raw_image = tf.read_file(image_path)
-            image = tf.image.decode_png(raw_image, channels=3)
+            image = tf.image.decode_jpeg(raw_image, channels=3)
+            label = tf.strings.to_number(label, out_type=tf.int32)
 
         with tf.name_scope('preprocessing'):
-            mean_channel = [125.30691805, 122.95039414, 113.86538318]
-            std_channel = [62.99321928, 62.08870764, 66.70489964]
+            mean_channel = [123.68, 116.779, 103.939]
             image = tf.cast(image, dtype=tf.float32)
             image = tf.subtract(image, mean_channel, name='mean_substraction')
-            image = tf.divide(image, std_channel, name='std_divide')
+            image = tf.image.resize(image, size=(256, 256))
 
         with tf.name_scope('data_augmentation'):
+            image = tf.image.random_crop(image, size=(227, 227, 3))
             image = tf.image.random_flip_left_right(image)
-            image = tf.image.random_brightness(image, max_delta=0.5)
+            image = tf.image.random_brightness(image, max_delta=20)
 
     return image, label
 
@@ -59,7 +60,7 @@ def _create_sample_cifar10(image_path, label):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Pipeline execution')
     parser.add_argument('dataset_csv', help='Path to the CSV decribing the dataset')
-    parser.add_argument('dataset_dir', help='Directory where the csv and the images folder are located')
+    parser.add_argument('images_dir', help='Path to the images directory')
     parser.add_argument('-l', '--logdir', default='~/tmp/aidl', help='Log dir for tfevents')
     parser.add_argument('-e', '--num_epochs', type=int, default=1, help='Number of epochs')
     parser.add_argument('-b', '--batch_size', type=int, default=5, help='Batch size')
@@ -68,7 +69,7 @@ if __name__ == '__main__':
 
     with tf.device('/cpu:0'):
         with tf.name_scope('input_pipeline'):
-            dataset = create_dataset(args.dataset_csv, args.dataset_dir, args.num_epochs, args.batch_size)
+            dataset = create_dataset(args.dataset_csv, args.images_dir, args.num_epochs, args.batch_size)
             iterator = dataset.make_one_shot_iterator()
             batch = iterator.get_next()
 
